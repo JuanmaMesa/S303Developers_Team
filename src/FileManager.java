ea917be2338f544c59dd77ddac2774512d56930d
JuanmaMesa <juanma_sme@hotmail.com>
2023-12-15 20:49:59 +0100

    añadimos ticket al Stockfile

--- src/FileManager.java
+++ src/FileManager.java
@@ -5,18 +5,21 @@ import java.io.PrintWriter;
 public class FileManager {
 
     public void saveData(FloristShop floristShop, String nameFile) throws IOException {
-        try (PrintWriter printWriter = new PrintWriter(new FileWriter(nameFile))){
+        try (PrintWriter printWriter = new PrintWriter(new FileWriter(nameFile))) {
 
             // guardar informacion en el archivo
             for (Product product : floristShop.getStock()) {
                 if (product instanceof Tree) {
-                    printWriter.println(("TREE," + product.toString()));
+                    printWriter.println((product.toString()));
                 } else if (product instanceof Flower) {
-                    printWriter.println(("FLOWER," + product.toString()));
+                    printWriter.println((product.toString()));
                 } else if (product instanceof Decoration) {
-                    printWriter.println(("DECORATION," + product.toString()));
+                    printWriter.println((product.toString()));
                 }
+            }
 
+            for(Ticket ticket : floristShop.getTickets()){
+                printWriter.println(ticket.toString());
             }
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
