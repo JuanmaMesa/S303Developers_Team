5cd35517f474f8ba225222826086f55e4cfcb444
JuanmaMesa <juanma_sme@hotmail.com>
2023-12-16 12:17:59 +0100

    listarRemove and confirmation

--- src/FileManager.java
+++ src/FileManager.java
@@ -5,21 +5,18 @@ import java.io.PrintWriter;
 public class FileManager {
 
     public void saveData(FloristShop floristShop, String nameFile) throws IOException {
-        try (PrintWriter printWriter = new PrintWriter(new FileWriter(nameFile))) {
+        try (PrintWriter printWriter = new PrintWriter(new FileWriter(nameFile))){
 
             // guardar informacion en el archivo
             for (Product product : floristShop.getStock()) {
                 if (product instanceof Tree) {
-                    printWriter.println((product.toString()));
+                    printWriter.println(("TREE," + product.toString()));
                 } else if (product instanceof Flower) {
-                    printWriter.println((product.toString()));
+                    printWriter.println(("FLOWER," + product.toString()));
                 } else if (product instanceof Decoration) {
-                    printWriter.println((product.toString()));
+                    printWriter.println(("DECORATION," + product.toString()));
                 }
-            }
 
-            for(Ticket ticket : floristShop.getTickets()){
-                printWriter.println(ticket.toString());
             }
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
